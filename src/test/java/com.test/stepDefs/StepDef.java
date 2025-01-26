6c1e99d90118b6fbb152a6cc86e2693f3a6df425
gabor.szabo <gabor.szabo.dzsi@gmail.com>
2025-01-26 19:15:35 +0100

    new class

---
+++ src/test/java/com.test/stepDefs/StepDef.java
@@ -0,0 +1,39 @@
+package com.test.stepDefs;
+
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class StepDef {
+
+    WebDriver driver;
+
+
+
+    @Before
+    public void setup() {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+        System.out.println("setupTest() method is running");
+
+
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
